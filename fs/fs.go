a21a357cc55b46b46a7baf847ee5ab6ca02c1022
Diyor Khaydarov <diyor28july@gmail.com>
2025-01-03 21:37:22 +0500

    feat: walk function

--- fs/fs.go
+++ fs/fs.go
@@ -1,6 +1,39 @@
 package fs
 
-import "os"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func listFiles(fsys fs.FS, dir string) ([]string, error) {
+	var fileList []string
+
+	entries, err := fs.ReadDir(fsys, dir)
+	if err != nil {
+		return nil, fmt.Errorf("error reading directory %q: %w", dir, err)
+	}
+
+	for _, entry := range entries {
+		path := filepath.Join(dir, entry.Name())
+		if entry.IsDir() {
+			subDirFiles, err := listFiles(fsys, path)
+			if err != nil {
+				return nil, err
+			}
+			fileList = append(fileList, subDirFiles...)
+		} else {
+			fileList = append(fileList, path)
+		}
+	}
+
+	return fileList, nil
+}
+
+func Walk(fsys fs.FS) ([]string, error) {
+	return listFiles(fsys, ".")
+}
 
 func FileExists(filename string) bool {
 	info, err := os.Stat(filename)
